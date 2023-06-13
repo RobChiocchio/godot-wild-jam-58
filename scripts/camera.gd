4a9e95f8e42318c96ada4c8cf2dd1b002ba6cea3
Rob Chiocchio <RobethX@users.noreply.github.com>
2023-06-13 17:06:00 +0200

    Optimization

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -7,24 +7,20 @@ var bound_offset = 80
 var pos_bound = self.position.x + bound_offset
 var neg_bound = self.position.x - bound_offset
 
-var axis = 1.0
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print(_player.position.x)
-	print(pos_bound)
-	print(neg_bound)
-			
-	var speed = get_node("../Player").velocity.x
+func _physics_process(delta):
+	var speed = _player.velocity.x
 	
-	if _player.position.x > pos_bound or _player.position.x < neg_bound:
-		pos_bound = lerp(pos_bound, _player.position.x + bound_offset, 0.1)
-		neg_bound = lerp(neg_bound, _player.position.x - bound_offset, 0.1)
+	#if _player.position.x > pos_bound or _player.position.x < neg_bound:
+	#	pos_bound = lerp(pos_bound, _player.position.x + bound_offset, 0.1)
+	#	neg_bound = lerp(neg_bound, _player.position.x - bound_offset, 0.1)
 		
-		target.x = lerp(target.x, _player.position.x, 0.20)
+	#	target.x = lerp(target.x, _player.position.x, 0.20)
+	#else:
+	target.x = _player.position.x
 
 	self.position.x = target.x
