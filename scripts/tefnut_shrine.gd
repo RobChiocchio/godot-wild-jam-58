9b2f017ca927dc24166913e9ddfa079c9a434dc4
Spades <46801977+Raycu@users.noreply.github.com>
2023-06-15 11:54:07 -0400

    Update tefnut_shrine.gd

--- scripts/tefnut_shrine.gd
+++ scripts/tefnut_shrine.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var _tefnut = self
 @onready var intact_collider = $intact_colllision
 @onready var destroyed_collider = $destroyed_collision
+@onready var destroyed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +16,11 @@ func _process(delta):
 
 func _on_left_rock_body_entered(body):
 	print(body.name)
-	if body.name == "tefnut_shrine":
+	if body.name == "tefnut_shrine" && destroyed == false:
 		_sprite.play("tefnut_leaves")
 		_tefnut.add_child(destroyed_collider)
 		_tefnut.remove_child(intact_collider)
+		destroyed = true
 		
 func _on_right_rock_body_entered(body):
 	_sprite.play("tefnut_leaves")
